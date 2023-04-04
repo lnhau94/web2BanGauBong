c8242ba62af4e247964d931b6b6e4e287e2dc42d
hau <lnhau94@gmail.com>
2023-04-04 15:29:28 +0700

    first code

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-    echo "hello world aaa";
+$server = 'webbear.mysql.database.azure.com';
+$username = 'lnhau';
+$password = 'lnhau94';
+$database = 'webbear';
+
+$conn = new mysqli($server, $username, $password, $database) ;
+  
+    if ($result = $conn -> query("SELECT * FROM Product p join Image i on p.productId = i.productId")) {
+        echo "Returned rows are: " . $result -> num_rows ."<br>";
+        $i = 0;
+        while($i++ < $result -> num_rows){
+          $row = $result -> fetch_array();
+          $j = 0;
+          echo $row[1]." " . $row[2]."<img src=./img/".$row[8].">";
+          echo "<br>";
+          
+        }
+        
+        // Free result set
+        $result -> free_result();
+      }
 ?>
